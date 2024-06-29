42d592530465a844be12f032e15c448e19586ca1
Nada <nada.amr1267@gmail.com>
2024-06-29 14:35:44 +0300

    building

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,5 @@
+
+class Constants {
+  static const String _key = "57ce31aca2d549b4a191ddf25811420c";
+  static const String apiKey = _key;
+}
--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:akhbar/core/utils/app_assets.dart';
 import 'package:akhbar/core/utils/app_colors.dart';
+import 'package:akhbar/features/home/widgets/drawer_app_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+   HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +22,47 @@ class HomeScreen extends StatelessWidget {
           title: Text(AppLocalizations.of(context)!.appName),
         ),
         // drawer: DrawerWidget(onDrawerItemClicked: onDrawerClick),
-        // body: bodyWidget,
+        body: bodyWidget,
       ),
     );
   }
+  late Widget bodyWidget;
+
+  // onDrawerClick(DrawerMenuItem item) {
+  //   switch (item) {
+  //     case DrawerMenuItem.category:
+  //       setState(() {
+  //         bodyWidget =  CategoryTab(onCategoryItemClicked: onCategoryItemClicked);
+  //         titleGlobal = "News";
+  //       });
+  //     case DrawerMenuItem.settings:
+  //       setState(() {
+  //         bodyWidget = const SettingsTab();
+  //         titleGlobal = "News";
+  //       });
+  //     case DrawerMenuItem.sources:
+  //       setState(() {
+  //         bodyWidget = SourcesTab();
+  //         titleGlobal = "News";
+  //       });
+  //   }
+  // }
+  // void onCategoryItemClicked (Category category) {
+  //   bodyWidget = CategoryDetails(category: category, onArticleItemClicked: onArticleItemClicked,);
+  //   setState(() {
+  //     titleGlobal = "News";
+  //   });
+  // }
+  //
+  // void onArticleItemClicked (Articles article) {
+  //   bodyWidget = ArticleDetails(article: article);
+  //   setState(() {
+  //     titleGlobal = article.title ?? "";
+  //   });
+  // }
+  //
+  // void onSourceItemClicked () {
+  //   bodyWidget = SourcesTab();
+  //   setState(() {});
+  // }
 }
