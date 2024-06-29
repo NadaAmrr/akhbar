08895a15f0728b9904bde841374e343d784c567d
Nada <nada.amr1267@gmail.com>
2024-06-29 20:50:27 +0300

    tab widget + provider

--- lib/features/news/widgets/tab_widget.dart
+++ lib/features/news/widgets/tab_widget.dart
@@ -1,10 +1,12 @@
-
 import 'package:akhbar/core/utils/app_colors.dart';
+import 'package:akhbar/features/news/provider/tab_provider.dart';
 import 'package:akhbar/features/news/widgets/source_widget.dart';
 import 'package:akhbar/models/SourcesResponse.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 class TabWidget extends StatelessWidget {
-  const TabWidget({
+  TabWidget({
     super.key,
     required this.sourcesList,
   });
@@ -15,23 +17,30 @@ class TabWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.symmetric(vertical: 15),
-        child:
-        DefaultTabController(
-            length: sourcesList.length,
-            child: TabBar(
-              dividerColor: Colors.transparent,
-              isScrollable: true,
-              indicatorColor: AppColors.white.withOpacity(0),
-              onTap: (value) {
-                // CategoryDetailsCubit.get(context).onSourceChange(value);
-              },
-              tabs: sourcesList.map((e) => Tab(
-                child: SourceItem(
-                  source: e,
-                  isSelected: true,
-                ),
-              )).toList(),
-            ))
-    );
+        child: ChangeNotifierProvider(
+            create: (BuildContext context) => TabProvider(),
+            builder: (context, child) {
+              var provider = Provider.of<TabProvider>(context);
+
+              return DefaultTabController(
+                  length: sourcesList.length,
+                  child: TabBar(
+                    dividerColor: Colors.transparent,
+                    isScrollable: true,
+                    indicatorColor: AppColors.white.withOpacity(0),
+                    onTap: (index) {
+                      provider.changeIndex(index);
+                    },
+                    tabs: sourcesList
+                        .map((e) => Tab(
+                              child: SourceItem(
+                                source: e,
+                                isSelected: provider.selectedIndex ==
+                                    sourcesList.indexOf(e),
+                              ),
+                            ))
+                        .toList(),
+                  ));
+            }));
   }
 }
